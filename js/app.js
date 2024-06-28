89eb6211f59b3831f1e6fc909e95e3b67ad66432
Fran Ruiz <francoruizgabriel@gmail.com>
2024-06-27 21:22:49 -0300

    probando API

--- js/app.js
+++ js/app.js
@@ -2,8 +2,43 @@ const container = document.querySelector('.container');
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
 
+const tempActual = document.querySelector('#tempActual');
+const tempMax = document.querySelector('#max');
+const tempMin = document.querySelector('#min');
+const st = document.querySelector('#st');
+const humedad = document.querySelector('#humedad');
+const viento = document.querySelector('#viento');
+const direccion = document.querySelector('#direccion');
+const nubocidad = document.querySelector('#nubocidad');
+const precipitacion = document.querySelector('#precipitacion');
+
+const btnMenu = document.querySelector('#btnMenu');
+const menu = document.querySelector('#menu');
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    
+
+    
+    
+});
+
 window.addEventListener('load', () => {
     formulario.addEventListener('submit', buscarClima);
+})
+// Despliegue del menu
+btnMenu.addEventListener('click', () => {
+    console.log('Me diste click - Corregir bug');
+    
+    menu.classList.toggle('hidden');
+    const desplegable =  document.querySelector('.hidden');
+    
+    if(!desplegable && btnMenu){
+       
+        menu2.classList.add('flex-col', 'space-y-2');
+    } else {
+        menu2.classList.remove('flex-col', 'space-y-2');
+    }
 })
 
 function buscarClima(e) {
@@ -74,11 +109,14 @@ function consultarAPI(ciudad, pais) {
 }
 
 function mostrarClima(datos) {
-    const {name, main: {temp, temp_max, temp_min}} = datos;
+    const {name, clouds: {all}, main: {temp, temp_max, temp_min, feels_like, humidity, pressure}, wind: {deg, speed}} = datos;
 
     const centigrados = kelvinACentigrados(temp);
     const max = kelvinACentigrados(temp_max);
     const min = kelvinACentigrados(temp_min);
+    const sensTerm = kelvinACentigrados(feels_like);
+    
+    
 
     const nombreCiudad = document.createElement('p');
     nombreCiudad.textContent = `Clima en ${name}`;
@@ -104,11 +142,56 @@ function mostrarClima(datos) {
     resultadoDiv.appendChild(tempMinima);
     
     resultado.appendChild(resultadoDiv);
+
+    //Funcion para conocer la dirección del viento
+    function direccionViento(gradoActual) {
+        
+        if(deg<=23 && deg>=338){
+            //Entonces el viento es del NORTE
+            return gradoActual = 'Norte';
+        } else if(deg>=24 && deg<=68) {
+            //Entonces el viento es del NORESTE
+            return gradoActual = 'Noreste';
+        } else if(deg>=69 && deg<=113) {
+            //Entonces el viento es del ESTE
+            return gradoActual = 'Este';
+        } else if(deg>=114 && deg<=157) {
+            //Entonces el viento es del SURESTE
+            return gradoActual = 'Sureste';
+        } else if(deg>=158 && deg<=202) {
+            //Entonces el viento es del SUR
+            return gradoActual = 'Sur';
+        } else if(deg>=203 && deg<=248) {
+            //Entonces el viento es del SUROESTE
+            return gradoActual = 'Suroeste';
+        } else if(deg>=249 && deg<=292) {
+            //Entonces el viento es del OESTE
+            return gradoActual = 'Oeste';
+        } else if(deg>=293 && deg<=337) {
+            //Entonces el viento es del NOROESTE
+            return gradoActual = 'Noroeste';
+        }
+    }
+    let gradoActual = direccionViento(deg);
+    console.log(gradoActual);
+
+    tempActual.innerHTML = `${centigrados} &#8451;`;
+    tempMax.innerHTML = `Máxima: ${max}°`;
+    tempMin.innerHTML = `Mínima: ${min}°`;
+    st.innerHTML = `ST: ${sensTerm}°`;
+    humedad.innerHTML = `Humedad: ${humidity}%`;
+    viento.innerHTML = `Viento: ${speed} k/h`;
+    direccion.innerHTML = `V.Dirección: ${gradoActual}`;
+    nubocidad.innerHTML = `Nubocidad: ${all}%`;
+    
+    
 }
 
 //Funcion comprimida para convertir grados kelvin de la API a centigrados 
 
 const kelvinACentigrados = grados => parseInt(grados - 273.15);
+
+
 
 function limpiarHTML() {
     while(resultado.firstChild) {
